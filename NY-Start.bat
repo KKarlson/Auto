ba2c2befdffe68eaa621419d022a1c8f6e24c750
KKarlson <kevinkarlson82@gmail.com>
2024-09-25 14:25:57 -0700

    Update NY-Start.bat

--- NY-Start.bat
+++ NY-Start.bat
@@ -1,4 +1,4 @@
-netsh wlan add profile filename="Wi-Fi-WPP-Internal.xml"
+netsh wlan add profile filename="Wi-Fi-Profile.xml"
 cd/d c:\
 powershell -Command "Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Force"
 powershell -executionPolicy Bypass -File d:\AutoV2Files\NGD-AutoCopy.ps1
