cc547cd38fb255130d1c7f7a7ce6596a2c8ce9fc
KKarlson <kevinkarlson82@gmail.com>
2024-09-26 13:22:39 -0700

    Update Start-X1CG12.bat

--- Start-X1CG12.bat
+++ Start-X1CG12.bat
@@ -1,6 +1,4 @@
 powershell -command "Get-ChildItem D:\Drivers\X1CG12WiFiDriver\ -Recurse -Filter '*inf' | ForEach-Object { PNPUtil.exe /add-driver $_.FullName /install }"
 net start wlansvc
 netsh wlan add profile filename="Wi-Fi-Profile.xml"
-cd/d c:\
-powershell -Command "Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Force"
 powershell -executionPolicy Bypass -File C:\TECH\Auto-main\AutoV3Files\NGD-Auto.ps1
