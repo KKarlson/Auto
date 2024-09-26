0f84ccd9c363fd8e4ac46559e7770a204036f612
KKarlson <kevinkarlson82@gmail.com>
2024-09-26 13:22:53 -0700

    Update Start-WiFi.bat

--- Start-WiFi.bat
+++ Start-WiFi.bat
@@ -1,4 +1,2 @@
 netsh wlan add profile filename="Wi-Fi-Profile.xml"
-cd/d c:\
-powershell -Command "Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Force"
 powershell -executionPolicy Bypass -File C:\TECH\Auto-main\AutoV3Files\NGD-Auto.ps1
