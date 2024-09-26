666762642acab3f81c6733975df3850d76b64a10
KKarlson <kevinkarlson82@gmail.com>
2024-09-26 10:21:28 -0700

    Update and rename NY-Start.bat to Start-WiFi.bat

--- Start-WiFi.bat
+++ Start-WiFi.bat
@@ -1,4 +1,4 @@
 netsh wlan add profile filename="Wi-Fi-Profile.xml"
 cd/d c:\
 powershell -Command "Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Force"
-powershell -executionPolicy Bypass -File d:\AutoV2Files\NGD-AutoCopy.ps1
+powershell -executionPolicy Bypass -File d:\AutoV2Files\NGD-Auto.ps1
