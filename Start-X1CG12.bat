17e1343207166d8002863ae6999a5e07407ab2c8
KKarlson <kevinkarlson82@gmail.com>
2024-09-26 10:24:09 -0700

    Update Start-X1CG12.bat

--- Start-X1CG12.bat
+++ Start-X1CG12.bat
@@ -3,4 +3,4 @@ net start wlansvc
 netsh wlan add profile filename="Wi-Fi-Profile.xml"
 cd/d c:\
 powershell -Command "Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Force"
-powershell -executionPolicy Bypass -File d:\AutoV2Files\NGD-AutoCopy.ps1
+powershell -executionPolicy Bypass -File d:\AutoV2Files\NGD-Auto.ps1
