b58380691436767e5cb2573659e2b2a21466e026
KKarlson <kevinkarlson82@gmail.com>
2024-09-26 10:24:23 -0700

    Update Start.bat

--- Start.bat
+++ Start.bat
@@ -1,3 +1,3 @@
 cd/d c:\
 powershell -Command "Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Force"
-powershell -executionPolicy Bypass -File d:\AutoV2Files\NGD-AutoCopy.ps1
+powershell -executionPolicy Bypass -File d:\AutoV2Files\NGD-Auto.ps1
