c63054fe7bb52c11388b5cbd3f830a22a727b189
KKarlson <kevinkarlson82@gmail.com>
2024-10-02 12:02:49 -0700

    Delete Start-7450.bat

--- Start-7450.bat
+++
@@ -1,4 +0,0 @@
-powershell -command "Get-ChildItem D:\Drivers\7450WiFiDriver\ -Recurse -Filter '*inf' | ForEach-Object { PNPUtil.exe /add-driver $_.FullName /install }"
-net start wlansvc
-netsh wlan add profile filename="Wi-Fi-Profile.xml"
-powershell -executionPolicy Bypass -File C:\TECH\Auto-main\AutoV3Files\NGD-Auto.ps1
